26643854e7a475a323b6c04da21afb6594ad3555
Rayipudi Pavan <pavanrayipudi1159@gmail.com>
2026-08-14 13:53:55 +0530

    feat: connect backend to mongodb

--- automation-api/src/server.js
+++ automation-api/src/server.js
@@ -1,8 +1,16 @@
 const app = require('./app');
 
 require('dotenv').config();
+const connectDB = require('./config/db');
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();

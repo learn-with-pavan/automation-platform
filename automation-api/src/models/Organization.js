1b85fbd84c0fb1ebfb8c2677dea3fc00b8b1fd77
Rayipudi Pavan <pavanrayipudi1159@gmail.com>
2026-08-14 19:08:01 +0530

    feat: added user and organization model

---
+++ automation-api/src/models/Organization.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const organizationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Organization", organizationSchema);
---
+++ automation-api/src/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ['owner', 'admin', 'viewer', 'editor'],
+        default: 'viewer'
+    },
+    organizationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Organization',
+        required: false,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+},
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('User', userSchema);
